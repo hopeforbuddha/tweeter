60cd87f0f12b8cfecbfdcf9d09e05f39d2c47205
Jacob <jacob.fitzgerald@outlook.com>
2021-06-17 02:19:35 +0000

    created togglable new-tweet section so you can hide it if you dont want to tweet

---
+++ public/scripts/hideOnClick.js
@@ -0,0 +1,14 @@
+$(document).ready(() => {
+  $(".arrowTweet").click(() => {
+
+    $(".new-tweet").toggle(500, "swing", () => {
+     
+    })
+  })
+  // $(".arrowTweet").click(() => {
+  //   $(".new-tweet").toggle(() => {
+  //     $(".new-tweet").slideDown("slow")
+  //   })
+  // })
+  
+})
